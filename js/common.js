d3e43c06ccd13838390b6e5f2c0680231f5357ec
xishifeng <1161850606@qq.com>
2017-06-23 18:28:06 +0800

    0623_f01

--- js/common.js
+++ js/common.js
@@ -1,3 +1,20 @@
+//移动端的适配工作.动态计算屏幕的宽度，从而得到网页的fontSize大小
+(function (doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function () {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            //if(clientWidth>750) clientWidth=750;
+            //这里限制最大的宽度尺寸，从而实现PC端的两边留白等。如果是pc站，一般不用这个限制
+            docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
+        };
+
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
+
 //字符串格式化
 String.prototype.stringFormat = function(){
 	var formatted = this;
@@ -8,10 +25,10 @@ String.prototype.stringFormat = function(){
 	return formatted;
 }
 
-//从location.href里获取参数 
+//从location.href里获取参数
 function getUrlParam(name) {
 	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
 	var r = decodeURI(window.location.search).substr(1).match(reg);
-	(r != null) && (return unescape(r[2]));
+	if(r != null) return unescape(r[2]);
 	return null;
 }
