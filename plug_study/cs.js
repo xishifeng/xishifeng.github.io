a1eb87d6813e8ec8504d17d05a7466032afabfbf
xishifeng <1161850606@qq.com>
2017-10-18 18:59:58 +0800

    1018_f01

---
+++ plug_study/cs.js
@@ -0,0 +1,7 @@
+console.log('xishifeng');
+//console.log(document.getElementById('kx'));
+//console.log(document.getElementById('todaysay'));
+//console.log(document.querySelector('a[onclick^="showWindow("]'));
+document.getElementById('kx').click();
+document.getElementById('todaysay').value='签到签到';
+document.querySelector('a[onclick^="showWindow(\'qwindow\', \'qiandao\', \'post\', \'0\');return false"]').click();
