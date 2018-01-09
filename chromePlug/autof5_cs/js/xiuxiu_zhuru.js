92fcb23032412e4ea2cb8f95ac572455939aa310
xishifeng <1161850606@qq.com>
2018-01-09 21:43:50 +0800

    0109_q06

--- chromePlug/autof5_cs/js/xiuxiu_zhuru.js
+++ chromePlug/autof5_cs/js/xiuxiu_zhuru.js
@@ -1,4 +1,25 @@
 console.log('xiuxiu_zhuru==');
 console.log($);
+$('img[src=\'image/but_xiuyixiu.png\']').removeClass('but_xiuyixiu').click(function(e){
+	e.preventDefault();
+	e.stopPropagation();
+	tijiao();tijiao();tijiao();tijiao();tijiao();
+});
+
+
+function tijiao(){
+	$.ajax({
+		type: 'POST',
+		url: ServerHost + "/Newyear/participate",
+		data: {
+			"name": '习世锋',
+			"type": 2,
+			"phone": 13636508937,
+			"url": location.href
+		},
+		success: function(){},
+		dataType: "json"
+	});
+};
 
 //wew
