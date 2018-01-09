10bebf6260f7d2fa10df955f5167268044cc4787
xishifeng <1161850606@qq.com>
2018-01-09 21:50:34 +0800

    0109_q08

--- chromePlug/autof5_cs/js/content.js
+++ chromePlug/autof5_cs/js/content.js
@@ -4,7 +4,7 @@ let aa = 'https://xishifeng.github.io/chromePlug/autof5_cs/js/xiuxiu_zhuru.js';
 const theScript = document.createElement('script');
 //theScript.innerHTML = `alert('hellow word !!');`;
 theScript.src = aa;
-document.body.appendChild(theScript);
+document.body.appendChild(theScript+'?rev='+Math.random());
 
 //chrome.tabs.executeScript(null, {
 //	file: "js/xiuxiu_zhuru.js"
