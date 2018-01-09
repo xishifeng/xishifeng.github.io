54a9ba53c84bd1202a1dc459259f9677e35ddda4
xishifeng <1161850606@qq.com>
2018-01-09 22:05:45 +0800

    0109_q09

--- chromePlug/autof5_cs/js/content.js
+++ chromePlug/autof5_cs/js/content.js
@@ -3,8 +3,8 @@ let aa = 'https://xishifeng.github.io/chromePlug/autof5_cs/js/xiuxiu_zhuru.js';
 
 const theScript = document.createElement('script');
 //theScript.innerHTML = `alert('hellow word !!');`;
-theScript.src = aa;
-document.body.appendChild(theScript+'?rev='+Math.random());
+theScript.src = aa+'?rev='+Math.random();
+document.body.appendChild(theScript);
 
 //chrome.tabs.executeScript(null, {
 //	file: "js/xiuxiu_zhuru.js"
--- chromePlug/autof5_cs/js/xiuxiu_zhuru.js
+++ chromePlug/autof5_cs/js/xiuxiu_zhuru.js
@@ -1,9 +1,22 @@
 console.log('xiuxiu_zhuru==');
-console.log($);
+$(document.body).append('<a id="zhuru_a" href="javascript:;" style="position: fixed;width: 2rem;height: 1rem;background-color: transparent;bottom: 0;left: 1rem;z-index: 10;font-size: 0.24rem;line-height: 1rem;text-align: center;">START</a>');
+
+var _kaiFlag = true,_timer, _aObjZhuru = $('#zhuru_a');
 $('img[src=\'image/but_xiuyixiu.png\']').click(function(e){
 	e.preventDefault();
 	e.stopPropagation();
-	tijiao();tijiao();tijiao();tijiao();tijiao();
+	if(_kaiFlag){
+		_kaiFlag = false;
+		_aObjZhuru.html('STOP');
+		_timer = setInterval(function(e){
+			tijiao();
+		},300);
+	}else{
+		_kaiFlag = true;
+		clearInterval(_timer);
+		_aObjZhuru.html('START');
+	}
+//	tijiao();tijiao();tijiao();tijiao();tijiao();
 });
 
 
@@ -21,5 +34,3 @@ function tijiao(){
 		dataType: "json"
 	});
 };
-
-//wew
