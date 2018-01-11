afaab57f728de6beb4c5367aefd1bfb794e950f0
xishifeng <1161850606@qq.com>
2018-01-11 13:19:29 +0800

    0111_q01

--- chromePlug/autoForumTaskNew/js/task.js
+++ chromePlug/autoForumTaskNew/js/task.js
@@ -6,8 +6,12 @@ switch(window.location.href) {
 	case 'http://cq.sanguosha.com/bbs/forum.php?mod=viewthread&tid=205614&extra=page%3D1&page=6000':
 		//forumGuanQianglouSp(6300, '论坛昵称：man17');//官方论坛特殊阶段抢楼
 		break;
-	case 'http://cq.sanguosha.com/bbs/forum.php?mod=viewthread&tid=206002&extra=page%3D1&page=6000':
-		forumGuanQianglou(5, '论坛昵称：man17'); //官方论坛抢楼，群雄争霸，勇闯虎牢关
+	case 'http://cq.sanguosha.com/bbs/forum.php?mod=viewthread&tid=206206&extra=page%3D1&page=6000':
+		forumGuanQianglou({
+			'minute': 10,
+			'floorWei': 8,
+			'content': '论坛昵称：man17'
+		}); //官方论坛抢楼，群雄争霸，勇闯虎牢关
 		break;
 	case 'http://cq.sanguosha.com/bbs/plugin.php?id=dsu_paulsign:sign':
 		forumGuanSign(); //官方论坛签到
@@ -24,12 +28,6 @@ switch(window.location.href) {
 		break;
 	case _shuilou360:
 		forum360Shuilou(); //360论坛灌水
-		break;
-	case 'https://tieba.baidu.com/p/5420056567?pn=1':
-		tiebaShuilou(); //三传百度贴吧灌水
-		break;
-	case 'https://tieba.baidu.com/p/5420056567?pn=1#':
-		tiebaShuilou(); //三传百度贴吧灌水
 		break;
 	default:
 		break;
@@ -59,35 +57,42 @@ function forumGuanQianglouSp(_num, _text) {
 		setTimeout(function() {
 			window.location.reload();
 		}, 150);
-	}
+	};
 };
 
-function forumGuanQianglou(_numWei, _text) {
+//function forumGuanQianglou(_numWei, _text) {
+function forumGuanQianglou(obj) {
 	//官方论坛抢楼
 	var _arr = document.querySelectorAll('a[id^="postnum"]>em');
-	var _lastNum = Number(_arr[_arr.length - 1].innerHTML);
-	console.log('qianglou==>' + _lastNum);
-	if(_arr.length < 10) {
-		var _domTemp = document.querySelectorAll('a.xw1');
-		if(_domTemp.length >= _numWei && _domTemp[_numWei - 1].innerHTML === 'man17') {
-			console.log('success ==> ' + _lastNum + '楼左右');
-			return false;
-		} else {
-			if((_lastNum + 1) % 10 === _numWei) {
-				document.querySelector('#fastpostmessage').innerText = _text;
-				document.querySelector('#fastpostsubmit').click();
-				window.location.reload();
+	if(_arr.length !== 0){
+		var _lastNum = Number(_arr[_arr.length - 1].innerHTML);
+		console.log('qianglou==>' + _lastNum);
+		if(_arr.length < 10) {
+			var _domTemp = document.querySelectorAll('a.xw1');
+			if(_domTemp.length >= obj['floorWei'] && _domTemp[obj['floorWei'] - 1].innerHTML === 'man17') {
+				console.log('success ==> ' + _lastNum + '楼左右');
+				return false;
 			} else {
-				setTimeout(function() {
+				if((_lastNum + 1) % 10 === obj['floorWei']) {
+					document.querySelector('#fastpostmessage').innerText = obj['content'];
+					document.querySelector('#fastpostsubmit').click();
 					window.location.reload();
-				}, 300);
+				} else {
+					setTimeout(function() {
+						window.location.reload();
+					}, 300);
+				};
 			};
+		} else {
+			setTimeout(function() {
+				window.location.reload();
+			}, 1500);
 		};
-	} else {
+	}else{
 		setTimeout(function() {
 			window.location.reload();
-		}, 1500);
-	}
+		}, obj['minute']*1000);
+	};
 };
 
 function forumGuanSign() {
@@ -96,7 +101,7 @@ function forumGuanSign() {
 		document.getElementById('kx').click();
 		document.getElementById('todaysay').value = '签到签到';
 		document.querySelector('a[onclick^="showWindow(\'qwindow\', \'qiandao\', \'post\', \'0\');return false"]').click();
-	}
+	};
 	location.href = _shuilouGuan;
 };
 
@@ -113,7 +118,7 @@ function forumGuanShuilou() {
 		}, 15001);
 	} else {
 		location.href = 'http://cq.sanguosha.com/bbs/plugin.php?id=yinxingfei_zzza:yinxingfei_zzza_hall';
-	}
+	};
 };
 
 function forumGuanYaoyiyao() {
@@ -124,12 +129,16 @@ function forumGuanYaoyiyao() {
 
 function forum360Sign() {
 	//360论坛签到
-	document.getElementById('daily_click').click();
-	console.log('qiandao success');
-	setTimeout(function(){
-		location.href = _shuilou360;
-	},3001)
-	
+	var _clickBtn = document.getElementById('daily_click');
+	if(_clickBtn.innerHTML == '已签到'){
+		console.log('task over');
+	}else{
+		document.getElementById('daily_click').click();
+		console.log('qiandao success');
+		setTimeout(function(){
+			location.href = _shuilou360;
+		},3001);
+	};
 };
 
 function forum360Shuilou() {
@@ -152,27 +161,6 @@ function forum360Shuilou() {
 		}
 	});
 };
-//
-//function tiebaShuilou(){
-//	chrome.runtime.sendMessage({
-//		call: 'listenTieba'
-//	}, function(response) {
-//		if(response['status'] === 'ok') {
-//			console.log(response['times'] + '==>' + response['timesTotal']);
-//			if(response['times'] <= response['timesTotal']) {
-//				document.querySelector('#ueditor_replace').innerHTML = '<p>本吧会员专属水楼，随意水~~~升级福音~|</p>'
-//				document.querySelector('[title=\'Ctrl+Enter快捷发表\']').click();
-//				setTimeout(function() {
-//					window.location.reload();
-//				}, 20001);
-//			} else {
-//				console.log(response['wenzi']);
-//			};
-//		} else {
-//			console.log(response['msg']);
-//		}
-//	});
-//};
 
 //官方论坛获取帖子楼层数dom最初的版本，
 //var _arr = document.querySelectorAll('div[id^="post_"] a[onclick="setCopy(this.href, \'帖子地址复制成功\');return false;"]>em');
