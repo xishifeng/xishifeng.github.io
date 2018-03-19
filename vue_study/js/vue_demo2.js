990c4c87a0269b28cbb5154a3750882413f2f7c9
xishifeng <1161850606@qq.com>
2018-03-20 00:21:27 +0800

    031903

--- vue_study/js/vue_demo2.js
+++ vue_study/js/vue_demo2.js
@@ -8,32 +8,32 @@ var _weekly_data = JSON.parse(decodeURIComponent(getUrlParam('weekly_data')));
 console.log(_weekly_data);
 
 Vue.component('simple-counter', {
-  template: '<button v-on:click="counter += 1">{{ counter }}</button>',
-  // 技术上 data 的确是一个函数了，因此 Vue 不会警告，
-  // 但是我们却给每个组件实例返回了同一个对象的引用
-  data: function () {
-    return {};
-  }
-})
+	props: ['data'],
+	template: '#myComponent',
+	data: function() {
+		return {};
+	}
+});
 
 var vm = new Vue({
 	el: '#app',
-	data: {
-		seen: true,
-		_weekly_data: JSON.parse(decodeURIComponent(getUrlParam('weekly_data')))
-	},
-	created: function(){
+	created: function() {
 		var _this = this;
 		$.ajax({
-			type:"get",
-			url:"js/sl.json",
-			async:true,
-			success: function(data){
+			type: "get",
+			url: "js/sl.json",
+			async: true,
+			success: function(data) {
 				console.log(data);
 				_this.$data.listMain = data.data;
 				console.log(_this.$data.listMain);
 			}
 		});
+	},
+	data: {
+		seen: true,
+		_weekly_data: JSON.parse(decodeURIComponent(getUrlParam('weekly_data'))),
+		listMain :[]
 	},
 	computed: {
 		// 仅读取
